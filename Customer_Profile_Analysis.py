ef23c41c2786e005968e3d66f24a9bd505bd1a07
Hugo Massashi Hatori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-06-28 19:37:36 -0300

    Update Customer_Profile_Analysis.py

--- Customer_Profile_Analysis.py
+++ Customer_Profile_Analysis.py
@@ -7,20 +7,14 @@ tabela = pd.read_csv("clientes.csv", encoding="latin1", sep=";")
 tabela = tabela.drop("Unnamed: 8", axis=1)
 display(tabela)
 
-
-
 #Passo 3: Tratamento de dados
 tabela["Salário Anual (R$)"] = pd.to_numeric(tabela["Salário Anual (R$)"], errors="coerce")
 tabela = tabela.dropna()
 print(tabela.info())
 
-
-
 #Passo 4: Análise inicial -> entender as notas do clientes (avaliar a média como parâmetro)
 display(tabela.describe())
 #A média das notas é de em torno de 52 (principal parâmetro de comparação)
-
-
 
 #Passo 5: Análise completa -> entender como a característica do cliente impacta na nota
 import plotly.express as px
@@ -28,22 +22,9 @@ import plotly.express as px
 for coluna in tabela.columns:
     #cria o gráfico
     grafico = px.histogram(tabela, x=coluna, y="Nota (1-100)", text_auto=True, histfunc='avg', nbins=10)
-
     #mostrar o gráfico
     grafico.show()
 
-
-
 #Correção de problema de exibição dos gráficos
 import plotly.io as pio
-
 pio.renderers.default='notebook'
-
-
-# ## Resultado da análise: perfil ideal do cliente
-# 
-# - Acima de 15 anos (não tem muita diferença entre as faixas etárias depois disso)
-# - Faixa salarial não parece fazer tanta diferença
-# - Áreas de trabalho: Entretenimento e Artista (evitar Construção)
-# - Tem entre 10 a 15 anos de experiência
-# - Com famílias não tão grandes (até no máximo 7 pessoas)
